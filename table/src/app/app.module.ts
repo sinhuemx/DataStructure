460f70654a8fd70177ad33e5107fde1fff7e9d68
Carlos Sinhue <>
2020-03-03 17:56:32 -0600

    datatable

--- table/src/app/app.module.ts
+++ table/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatatableComponent } from './components/datatable/datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatatableComponent
   ],
   imports: [
     BrowserModule,
